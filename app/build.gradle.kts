621d9d2bbf3c04c86439b55c7e3d351b4bdff805
Alex Vanyo <vanyoalex@gmail.com>
2021-03-21 22:36:26 -0500

    Update app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,36 +1,35 @@
 plugins {
-    id 'com.android.application'
-    id 'kotlin-android'
+    id("com.android.application")
+    kotlin("android")
 }
 
 android {
-    compileSdkVersion 30
+    compileSdkVersion(30)
 
     defaultConfig {
-        applicationId "com.android.example.lint_usage"
-        minSdkVersion 21
-        targetSdkVersion 30
-        versionCode 1
-        versionName "1.0"
+        applicationId = "com.android.example.lint_usage"
+        minSdkVersion(21)
+        targetSdkVersion(30)
+        versionCode = 1
+        versionName = "1.0"
     }
 
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_1_8
-        targetCompatibility JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = '1.8'
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
     }
     lintOptions {
-        textReport true
+        textReport = true
 
         // Produce report for CI:
         // https://docs.github.com/en/github/finding-security-vulnerabilities-and-errors-in-your-code/sarif-support-for-code-scanning
-        sarifOutput file("../lint-results.sarif")
+        sarifOutput = file("../lint-results.sarif")
     }
 }
 
 dependencies {
-    implementation project(':library')
+    implementation(project(":library"))
 }
-
