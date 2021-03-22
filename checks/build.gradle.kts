41ac17fa34d374e735f4cd37a53f9d53c79fa74b
Alex Vanyo <vanyoalex@gmail.com>
2021-03-21 22:41:38 -0500

    Update checks/build.gradle.kts

--- checks/build.gradle.kts
+++ checks/build.gradle.kts
@@ -1,26 +1,24 @@
-apply plugin: 'java-library'
-apply plugin: 'kotlin'
-apply plugin: 'com.android.lint'
+plugins {
+    kotlin("jvm")
+    id("com.android.lint")
+}
 
 lintOptions {
-    htmlReport true
-    htmlOutput file("lint-report.html")
-    textReport true
-    absolutePaths false
-    ignoreTestSources true
+    htmlReport = true
+    htmlOutput = file("lint-report.html")
+    textReport = true
+    isAbsolutePaths = false
+    isIgnoreTestSources = true
 }
 
 dependencies {
     // For a description of the below dependencies, see the main project README
-    compileOnly "com.android.tools.lint:lint-api:${Versions.lint}"
+    compileOnly("com.android.tools.lint:lint-api:${Versions.lint}")
     // You typically don't need this one:
-    compileOnly "com.android.tools.lint:lint-checks:${Versions.lint}"
-    compileOnly "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
+    compileOnly("com.android.tools.lint:lint-checks:${Versions.lint}")
+    compileOnly(kotlin("stdlib"))
 
-    testImplementation "junit:junit:4.13.2"
-    testImplementation "com.android.tools.lint:lint:${Versions.lint}"
-    testImplementation "com.android.tools.lint:lint-tests:${Versions.lint}"
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("com.android.tools.lint:lint:${Versions.lint}")
+    testImplementation("com.android.tools.lint:lint-tests:${Versions.lint}")
 }
-
-sourceCompatibility = "1.8"
-targetCompatibility = "1.8"
