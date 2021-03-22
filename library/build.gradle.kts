36ad263b7f1f63e85a922422addf51b8f0ffdc11
Alex Vanyo <vanyoalex@gmail.com>
2021-03-21 22:37:47 -0500

    Update library/build.gradle.kts

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,18 +1,20 @@
-apply plugin: 'com.android.library'
+plugins {
+    id("com.android.library")
+}
 
 android {
-    compileSdkVersion 30
+    compileSdkVersion(30)
     defaultConfig {
-        minSdkVersion 15
-        targetSdkVersion 30
+        minSdkVersion(15)
+        targetSdkVersion(30)
     }
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_1_8
-        targetCompatibility JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
 
 /** Package the given lint checks library into this AAR  */
 dependencies {
-    lintPublish project(':checks')
+    lintPublish(project(":checks"))
 }
